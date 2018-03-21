b3cfa6cb270fbe667b8523460e0b57ebb8a66a2f
Gabriel Hochstatter <gabrielhochstatter@gmail.com>
2018-03-21 15:04:28 +0000

    finished challenge 15

--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,44 @@
 class DockingStation
-    def release_bike
-    end 
-end 
 
-class Bike 
+    attr_reader :bikerack
+
+
+    def initialize(bike = nil)
+
+      @bikerack = []
+      deposit_bike(bike) if bike
+
+    end
+
+    def release_bike
+      raise "No bikes available" if bikerack_empty?
+      @bikerack.pop
+    end
+
+    def dock(bike)
+      raise "Station is full" if bikerack_full?
+      deposit_bike(bike)
+      return bike
+    end
+
+    private
+
+
+    def bikerack_full?
+      @bikerack.size >= 20
+    end
+
+    def bikerack_empty?
+      @bikerack.empty?
+    end
+
+    def deposit_bike(bike)
+      @bikerack << bike
+    end
+
+end
+
+class Bike
     def working?
-    end 
-end 
+    end
+end
--- spec/docking_spec.rb
+++ spec/docking_spec.rb
@@ -1,7 +1,42 @@
 require 'app'
 
-describe 'DockingStation' do 
-    it 'responds to release_bike' do 
+describe 'DockingStation' do
+    it 'responds to release_bike' do
         expect(DockingStation.new).to respond_to(:release_bike)
-    end 
+    end
+
+    it 'lets you dock a bike' do
+      expect(DockingStation.new).to respond_to(:dock).with(1).argument
+    end
+
+    it 'docks something' do
+      bike = Bike.new
+      expect(DockingStation.new.dock(bike)).to eq bike
+    end
+
+    it 'starts empty' do
+      expect(DockingStation.new.bikerack).to be_empty
+    end
+
+    describe '#release_bike' do
+      it 'raises error when no bikes' do
+        expect { DockingStation.new.release_bike }.to raise_error 'No bikes available'
+      end
+
+      it "returns last bike if there are any in the station" do
+        test_bike = Bike.new
+        expect(DockingStation.new(test_bike).release_bike).to eq test_bike
+      end
+    end
+
+    describe '#dock' do
+      it 'raises error when someone tries to dock a bike at a full station (20 bikes)' do
+        test_station = DockingStation.new(Bike.new)
+        test_bike = Bike.new
+        19.times do test_station.dock(Bike.new) end
+        expect { test_station.dock(test_bike) }.to raise_error 'Station is full'
+      end
+
+    end
+
 end
